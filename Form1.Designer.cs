0e4ea8c31f076c9d088a9508872bc17e67b37d45
Jean Carlos Guzman Jose <jeancarlosg93@gmail.com>
2024-04-15 23:13:08 -0400

    add controls for images ingredients and nutrition facts

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,89 +28,162 @@
         /// </summary>
         private void InitializeComponent()
         {
-            listBox1 = new ListBox();
+            listSelection = new ListBox();
             comboBox1 = new ComboBox();
             lblName = new Label();
             groupBox1 = new GroupBox();
+            lblIngredient = new Label();
+            pictureBox1 = new PictureBox();
+            txtNutritionInfo = new Label();
+            listIngredient = new ListBox();
             txtDescription = new TextBox();
+            listNutritionInfo = new ListBox();
             groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
-            // listBox1
+            // listSelection
             // 
-            listBox1.Font = new Font("Segoe UI", 11.1428576F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            listBox1.ForeColor = SystemColors.ActiveCaptionText;
-            listBox1.FormattingEnabled = true;
-            listBox1.HorizontalScrollbar = true;
-            listBox1.ItemHeight = 36;
-            listBox1.Location = new Point(21, 98);
-            listBox1.Margin = new Padding(5, 6, 5, 6);
-            listBox1.Name = "listBox1";
-            listBox1.ScrollAlwaysVisible = true;
-            listBox1.Size = new Size(470, 652);
-            listBox1.TabIndex = 0;
-            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            listSelection.Font = new Font("Segoe UI", 11.1428576F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            listSelection.ForeColor = SystemColors.ActiveCaptionText;
+            listSelection.FormattingEnabled = true;
+            listSelection.HorizontalScrollbar = true;
+            listSelection.ItemHeight = 20;
+            listSelection.Location = new Point(12, 49);
+            listSelection.Name = "listSelection";
+            listSelection.Size = new Size(276, 484);
+            listSelection.TabIndex = 0;
+            listSelection.SelectedIndexChanged += listBox1_SelectedIndexChanged;
             // 
             // comboBox1
             // 
-            comboBox1.Location = new Point(21, 40);
-            comboBox1.Margin = new Padding(5, 6, 5, 6);
+            comboBox1.Location = new Point(12, 20);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(205, 38);
+            comboBox1.Size = new Size(276, 23);
             comboBox1.TabIndex = 1;
             comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
             // 
             // lblName
             // 
             lblName.AutoSize = true;
-            lblName.Location = new Point(23, 49);
+            lblName.Location = new Point(13, 24);
+            lblName.Margin = new Padding(2, 0, 2, 0);
             lblName.Name = "lblName";
-            lblName.Size = new Size(69, 30);
+            lblName.Size = new Size(39, 15);
             lblName.TabIndex = 2;
             lblName.Text = "Name";
             // 
             // groupBox1
             // 
+            groupBox1.Controls.Add(listNutritionInfo);
+            groupBox1.Controls.Add(lblIngredient);
+            groupBox1.Controls.Add(pictureBox1);
+            groupBox1.Controls.Add(txtNutritionInfo);
+            groupBox1.Controls.Add(listIngredient);
             groupBox1.Controls.Add(txtDescription);
             groupBox1.Controls.Add(lblName);
-            groupBox1.Location = new Point(516, 87);
+            groupBox1.Location = new Point(301, 44);
+            groupBox1.Margin = new Padding(2);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(731, 663);
+            groupBox1.Padding = new Padding(2);
+            groupBox1.Size = new Size(402, 496);
             groupBox1.TabIndex = 4;
             groupBox1.TabStop = false;
+            // 
+            // lblIngredient
+            // 
+            lblIngredient.AutoSize = true;
+            lblIngredient.Location = new Point(13, 211);
+            lblIngredient.Margin = new Padding(2, 0, 2, 0);
+            lblIngredient.Name = "lblIngredient";
+            lblIngredient.Size = new Size(66, 15);
+            lblIngredient.TabIndex = 9;
+            lblIngredient.Text = "Ingredients";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.ActiveBorder;
+            pictureBox1.Location = new Point(221, 41);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(168, 158);
+            pictureBox1.TabIndex = 8;
+            pictureBox1.TabStop = false;
+            // 
+            // txtNutritionInfo
+            // 
+            txtNutritionInfo.AutoSize = true;
+            txtNutritionInfo.Location = new Point(221, 211);
+            txtNutritionInfo.Margin = new Padding(2, 0, 2, 0);
+            txtNutritionInfo.Name = "txtNutritionInfo";
+            txtNutritionInfo.Size = new Size(121, 15);
+            txtNutritionInfo.TabIndex = 7;
+            txtNutritionInfo.Text = "Nutrition Information";
+            // 
+            // listIngredient
+            // 
+            listIngredient.BackColor = SystemColors.ButtonFace;
+            listIngredient.BorderStyle = BorderStyle.None;
+            listIngredient.Font = new Font("Segoe UI", 11.1428576F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            listIngredient.ForeColor = SystemColors.ActiveCaptionText;
+            listIngredient.FormattingEnabled = true;
+            listIngredient.ItemHeight = 20;
+            listIngredient.Location = new Point(13, 229);
+            listIngredient.Name = "listIngredient";
+            listIngredient.Size = new Size(194, 260);
+            listIngredient.TabIndex = 5;
             // 
             // txtDescription
             // 
             txtDescription.BackColor = SystemColors.ButtonFace;
             txtDescription.BorderStyle = BorderStyle.FixedSingle;
-            txtDescription.Location = new Point(23, 82);
+            txtDescription.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtDescription.Location = new Point(13, 41);
+            txtDescription.Margin = new Padding(2);
             txtDescription.Multiline = true;
             txtDescription.Name = "txtDescription";
-            txtDescription.Size = new Size(331, 171);
+            txtDescription.Size = new Size(194, 158);
             txtDescription.TabIndex = 3;
+            // 
+            // listNutritionInfo
+            // 
+            listNutritionInfo.BackColor = SystemColors.ButtonFace;
+            listNutritionInfo.BorderStyle = BorderStyle.None;
+            listNutritionInfo.Font = new Font("Segoe UI", 11.1428576F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            listNutritionInfo.ForeColor = SystemColors.ActiveCaptionText;
+            listNutritionInfo.FormattingEnabled = true;
+            listNutritionInfo.ItemHeight = 20;
+            listNutritionInfo.Location = new Point(221, 231);
+            listNutritionInfo.Name = "listNutritionInfo";
+            listNutritionInfo.Size = new Size(168, 260);
+            listNutritionInfo.TabIndex = 10;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1296, 900);
+            ClientSize = new Size(716, 551);
             Controls.Add(groupBox1);
             Controls.Add(comboBox1);
-            Controls.Add(listBox1);
-            Margin = new Padding(5, 6, 5, 6);
+            Controls.Add(listSelection);
             Name = "Form1";
             Text = "Form1";
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
-        private ListBox listBox1;
+        private ListBox listSelection;
         private ComboBox comboBox1;
         private Label lblName;
         private GroupBox groupBox1;
         private TextBox txtDescription;
+        private ListBox listIngredient;
+        private Label txtNutritionInfo;
+        private PictureBox pictureBox1;
+        private Label lblIngredient;
+        private ListBox listNutritionInfo;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -5,16 +5,18 @@ using System.Runtime.CompilerServices;
 using CsvHelper.Configuration;
 using CsvHelper.Configuration.Attributes;
 using System.Linq;
+using System;
 
 namespace listboxtest
 {
     public partial class Form1 : Form
     {
         List<Meal> meals = new List<Meal>();
-
+       
         public Form1()
 
         {
+            
             InitializeComponent();
             comboBox1.Text = "Showing all options";
             using (StreamReader inputfile =
@@ -37,7 +39,7 @@ namespace listboxtest
 
                         }
 
-                        listBox1.Items.Add(record.Name);
+                        listSelection.Items.Add(record.Name);
 
 
 
@@ -45,47 +47,82 @@ namespace listboxtest
                 }
             }
         }
-
+        
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            int index = listBox1.SelectedIndex;
-            var newitem = listBox1.SelectedItem;
+            
+            int index = listSelection.SelectedIndex;
+            var newitem = listSelection.SelectedItem;
+            listIngredient.Items.Clear();
+            listNutritionInfo.Items.Clear();
 
-            for (int i = 0; i < meals.Count; i++)
-            {
-                if (newitem.Equals(meals[i].Name))
-                {
-                    txtDescription.Text = meals[i].Description;
-                    lblName.Text = meals[i].Name;
-                }
-            }
-
-        }
-
-            private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-            {
-
-                listBox1.Items.Clear();
-                var newitem = comboBox1.SelectedItem;
 
                 for (int i = 0; i < meals.Count; i++)
                 {
-                    if (newitem.Equals(meals[i].Type))
+                    if (newitem.Equals(meals[i].Name))
                     {
-                        //this is a shortcut using the "select" method from the "list" class
-                        //it's using linq language to search on the list for all the items
-                        //that contain type =="" 
-                        //meals.Select(a => a.Type == "");
-                        listBox1.Items.Add(meals[i].Name);
+                        lblName.Text = meals[i].Name;
+                        txtDescription.Text = meals[i].Description;
+                        listIngredient.Items.Add(meals[i].Ingredient1);
+                        listIngredient.Items.Add(meals[i].Ingredient2);
+                        listIngredient.Items.Add(meals[i].Ingredient3);
+                        listIngredient.Items.Add(meals[i].Ingredient4);
+                        listIngredient.Items.Add(meals[i].Ingredient5);
+                        listIngredient.Items.Add(meals[i].Ingredient6);
+                        listIngredient.Items.Add(meals[i].Ingredient7);
+                        listIngredient.Items.Add(meals[i].Ingredient8);
+                        listIngredient.Items.Add(meals[i].Ingredient9);
+
+                        listNutritionInfo.Items.Add("Calories:\t" + meals[i].Calories);
+                        listNutritionInfo.Items.Add("Fat:\t" + meals[i].Fat);
+                        listNutritionInfo.Items.Add("Carbs:\t" + meals[i].Carbs);
+                        listNutritionInfo.Items.Add("Protein:\t" + meals[i].Protein);
+
 
                     }
                 }
+            
+           
 
-            }
+        }
         
-    
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
 
-    private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+            listSelection.Items.Clear();
+            var newitem = comboBox1.SelectedItem;
+            try
+            {
+
+            
+            for (int i = 0; i < meals.Count; i++)
+            {
+                if (newitem.Equals(meals[i].Type))
+                {
+                    //this is a shortcut using the "select" method from the "list" class
+                    //it's using linq language to search on the list for all the items
+                    //that contain type =="" 
+                    //meals.Select(a => a.Type == "");
+                    listSelection.Items.Add(meals[i].Name);
+                }
+            }
+            }
+            catch (NullReferenceException)
+            {
+                txtDescription.Text = "";
+                throw;
+            }
+
+        }
+
+
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint_1(object sender, PaintEventArgs e)
         {
 
         }
